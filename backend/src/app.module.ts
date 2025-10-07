7928fccac08f9f6044d061475202bd9db0155a90
Jorge Rivera <jorge-421@hotmail.com>
2025-10-07 02:15:55 -0600

    complete nestjs api

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from '@/primsa/prisma.module';
+import { BookingsModule } from './bookings/bookings.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    AuthModule,
+    BookingsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
